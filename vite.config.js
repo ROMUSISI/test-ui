b31888b738c65807b72cdf5a91ad7323f06e3f62
Musisi <musisironald2025@gmail.com>
2025-06-27 01:00:14 +0300

    corrected the vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',       // ensures the build output goes into the 'dist' folder
+    emptyOutDir: true     // cleans the folder before building
+  }
 })
